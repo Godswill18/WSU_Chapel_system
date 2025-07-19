88f2a23dd227ae56ac28aeb26ca4e46970a7eea5
Godswill <gokhamera@gmail.com>
2025-07-19 09:47:43 +0100

    14th commit

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose.js';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import connectDB from '../DB/connectMongoDB.js';
