c3039b79b8a243a2a75ff341636e1b398829b5fb
Godswill Okhamera <105759150+Godswill18@users.noreply.github.com>
2025-07-19 17:14:04 +0100

    Delete Backend directory

--- Backend/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getDashboardStats } from '../controllers/statsController.js';
-const router = express.Router();
-
-// You can add authentication middleware here if needed
-router.get('/stats', getDashboardStats);
-
-export default router;
