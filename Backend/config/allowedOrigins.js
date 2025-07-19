0162f5e2b0aa5fc27a1194fdd5ef72e8912b9aa6
Godswill <gokhamera@gmail.com>
2025-07-19 16:17:46 +0100

    latest 4

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3000',
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'https://chapel-mz4cve0l7-gokhameragmailcoms-projects.vercel.app'
 ];
 
 export default allowedOrigins;
