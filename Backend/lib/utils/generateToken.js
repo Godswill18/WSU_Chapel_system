26d28c043c79c523363bb2c9893c896db24f9e0d
Godswill <gokhamera@gmail.com>
2025-07-19 23:03:18 +0100

    cookies fix

--- Backend/lib/utils/generateToken.js
+++ Backend/lib/utils/generateToken.js
@@ -9,7 +9,8 @@ export const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 2 * 24 * 60 * 60 * 1000, // Cookie expiration time (2 days)
     httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
     sameSite: "strict", // Helps prevent CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in development mode but change when in production in ENV file
+    secure: true, // Use secure cookies in development mode but change when in production in ENV file
+    // secure: process.env.NODE_ENV !== "development", // Use secure cookies in development mode but change when in production in ENV file
   })  
   return token; // Return the generated token
 }
